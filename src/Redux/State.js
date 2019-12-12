29fd21daaeb25e3e3d14ca5359efb7d0b11df660
vladimir <vladim.volovenko@gmail.com>
2019-12-12 15:35:54 +0200

    store was create as oop-object and changed path for props in index.js and app.js

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,71 +1,80 @@
-let renderEntireTree = () => {
-    console.log('state changed');
-}
-
-let state = {
-    ProfilePage: {
-        PostData: [
-            {id: 1, message: 'hi!', likesCount: 4},
-            {id: 2, message: 'hello!', likesCount: 3},
-            {id: 3, message: 'O`key!!', likesCount: 5},
-            {id: 4, message: 'hello my dear frшутв!!!', likesCount: 5}
-        ],
-        newPostData: 'Привет, как дела?'
+let store = {
+    _state: {
+        ProfilePage: {
+            PostData: [
+                {id: 1, message: 'hi!', likesCount: 4},
+                {id: 2, message: 'hello!', likesCount: 3},
+                {id: 3, message: 'O`key!!', likesCount: 5},
+                {id: 4, message: 'hello my dear frшутв!!!', likesCount: 5}
+            ],
+            newPostData: 'Привет, как дела?'
+        },
+        dialogsPage: {
+            DialogsData: [
+                {name: 'Dimych', id: 1},
+                {name: 'Andrey', id: 2},
+                {name: 'Sveta', id: 3},
+                {name: 'Sasha', id: 4},
+                {name: 'Valera', id: 5}
+            ],
+            MessagesData: [
+                {message: 'hi', id: 1},
+                {message: 'hello!', id: 2},
+                {message: 'o`key!', id: 3},
+                {message: 'Let`s go', id: 4},
+                {message: 'Yo-ho!', id: 5}
+            ],
+            newmes: 'хорошо!'
+        }
     },
-    dialogsPage: {
-        DialogsData: [
-            {name: 'Dimych', id: 1},
-            {name: 'Andrey', id: 2},
-            {name: 'Sveta', id: 3},
-            {name: 'Sasha', id: 4},
-            {name: 'Valera', id: 5}
-        ],
-        MessagesData: [
-            {message: 'hi', id: 1},
-            {message: 'hello!', id: 2},
-            {message: 'o`key!', id: 3},
-            {message: 'Let`s go', id: 4},
-            {message: 'Yo-ho!', id: 5}
-        ],
-        newmes: 'хорошо!'
+
+    getState () {
+        return this._state;
+    },
+
+    _callSubscriber () {
+        console.log('state changed');
+    },
+
+    addPost () {
+        let newPost = {
+            id: 5,
+            message: this._state.ProfilePage.newPostData,
+            likesCount: 0
+        };
+        this._state.ProfilePage.PostData.push(newPost);
+        this._state.ProfilePage.newPostData = '';
+        this._callSubscriber(this._state);
+    },
+
+    updatePostData (newText) {
+        this._state.ProfilePage.newPostData = newText;
+        this._callSubscriber(this._state);
+    },
+
+    addMessage () {
+        let NewMessage = {
+            id: 6,
+            message: this._state.dialogsPage.newmes
+        }
+        this._state.dialogsPage.MessagesData.push(NewMessage);
+        this._state.dialogsPage.newmes = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateMessage (newText) {
+        this._state.dialogsPage.newmes = newText;
+        this._callSubscriber(this._state);
+    },
+    
+    
+    subscribe (observer) {// pattern for avoid cyclic dependence
+
+        this._callSubscriber = observer;//renderEntireTree - global 
     }
+
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.ProfilePage.newPostData,
-        likesCount: 0
-    };
-    state.ProfilePage.PostData.push(newPost);
-    state.ProfilePage.newPostData = '';
-    renderEntireTree(state);
-}
-
-export let updatePostData = (newText) => {
-    state.ProfilePage.newPostData = newText;
-    renderEntireTree(state);
-}
-
-export let addMessage = () => {
-    let NewMessage = {
-        id: 6,
-        message: state.dialogsPage.newmes
-    }
-    state.dialogsPage.MessagesData.push(NewMessage);
-    state.dialogsPage.newmes = '';
-    renderEntireTree(state);
-}
-
-export let updateMessage = (newText) => {
-    state.dialogsPage.newmes = newText;
-    renderEntireTree(state);
-}
-
-export const subscribe = (observer) => {// pattern for avoid cyclic dependence
-
-    renderEntireTree = observer;//renderEntireTree - global 
-}
-
-export default state;
+export default store;
+window.store = store;
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './Redux/State';
+import store from './Redux/State';
 import App from './App';
-import {addPost} from './Redux/State';
-import {updatePostData} from './Redux/State';
-import {addMessage} from './Redux/State';
-import {updateMessage} from './Redux/State';
+
 
 // addPost('hoho');
 let renderEntireTree = (state) => {
 ReactDOM.render(
   <App state={state} 
-       addPost={addPost} 
-       updatePostData={updatePostData} 
-       addMessage={addMessage} 
-       updateMessage={updateMessage} />
+       addPost={store.addPost.bind(store)} 
+       updatePostData={store.updatePostData.bind(store)} 
+       addMessage={store.addMessage.bind(store)} 
+       updateMessage={store.updateMessage.bind(store)} />
     , document.getElementById('root'));
 }
 
-renderEntireTree(state);
+renderEntireTree(store.getState());
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
